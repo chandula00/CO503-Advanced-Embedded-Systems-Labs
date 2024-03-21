19e8ea1f07efbfd0493d2b0e9ace26b532885730
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2024-03-21 18:53:02 +0530

    first commit

---
+++ lab01/part01/software/SoC_new_OS/obj/default/counter.d
@@ -0,0 +1,6 @@
+obj/default/counter.o: counter.c ../SoC_new_OS_bsp//HAL/inc/io.h \
+ ../SoC_new_OS_bsp//HAL/inc/alt_types.h
+
+../SoC_new_OS_bsp//HAL/inc/io.h:
+
+../SoC_new_OS_bsp//HAL/inc/alt_types.h:
---
+++ lab01/part01/software/test/obj/default/counter.d
@@ -0,0 +1,6 @@
+obj/default/counter.o: counter.c ../test_bsp//HAL/inc/io.h \
+ ../test_bsp//HAL/inc/alt_types.h
+
+../test_bsp//HAL/inc/io.h:
+
+../test_bsp//HAL/inc/alt_types.h:
