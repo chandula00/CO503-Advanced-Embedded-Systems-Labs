3ec572243938d2e9a6649b115d1554ca08c83e07
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2024-04-23 12:45:32 +0530

    save

--- practical2/software/CI_SoC/obj/default/ci_crc.d
+++
@@ -1,6 +0,0 @@
-obj/default/ci_crc.o: ci_crc.c ../CI_SoC_bsp/system.h \
- ../CI_SoC_bsp/linker.h
-
-../CI_SoC_bsp/system.h:
-
-../CI_SoC_bsp/linker.h:
--- practical2/software/CRC_P2/obj/default/ci_crc.d
+++
@@ -1,6 +0,0 @@
-obj/default/ci_crc.o: ci_crc.c ../CRC_P2_bsp/system.h \
- ../CRC_P2_bsp/linker.h
-
-../CRC_P2_bsp/system.h:
-
-../CRC_P2_bsp/linker.h:
