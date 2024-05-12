6819a8eb5302342d9eae60a4971e365ac487d746
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2024-05-12 20:40:37 +0530

    task 1 working

---
+++ lab03-part1/software/Consumer_2/obj/default/FIFO_1.d
@@ -0,0 +1,13 @@
+obj/default/FIFO_1.o: FIFO_1.c FIFO_1.h ../Consumer_2_bsp//HAL/inc/io.h \
+ ../Consumer_2_bsp//HAL/inc/alt_types.h ../Consumer_2_bsp/system.h \
+ ../Consumer_2_bsp/linker.h
+
+FIFO_1.h:
+
+../Consumer_2_bsp//HAL/inc/io.h:
+
+../Consumer_2_bsp//HAL/inc/alt_types.h:
+
+../Consumer_2_bsp/system.h:
+
+../Consumer_2_bsp/linker.h:
